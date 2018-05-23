7e5d8cd56cb4695570268743814ba90aeb845697
“henryyang0820git config --global user.email “y1045033116@qq.com <1045033116@qq.com>
2018-05-23 15:06:39 +0800

    更新数据

--- forTime.py
+++ forTime.py
@@ -7,9 +7,10 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
+import os
 
 #邮件数据
-mail = {'me':'309506489@qq.com','host':'smtp.qq.com','port':'465','pw':'lhiawwpunkcrbjeg'}
+mail = {'me':'309506489@qq.com','host':'smtp.qq.com','port':'465','pw':'qisgaudsxbunbhef'}
 # 多个收件人用list,单个收件人字符串
 # accpter = ["1045033116@qq.com","309506489@qq.com","1031937206@qq.com"]
 accpter = ['1045033116@qq.com','18612404428@163.com','likelin_work@163.com']
@@ -17,10 +18,13 @@ accpter = ['1045033116@qq.com','18612404428@163.com','likelin_work@163.com']
 #每次循环等待间隔时间,默认60秒程序唤醒一次
 waitTime = 60
 #需要定时监测的时间点
-timeList = ['10:30','11:30','13:45','14:30','14:50']
+timeList = ['11:30','13:45','14:45']
 # timeList = ['20:07','20:09','20:11']
 #基金代码
 fundCode =['161725','000311','110022','161616','486001','002611']
+#路径
+base_path = os.path.dirname(os.path.abspath(__file__))
+excel_path = os.path.abspath(base_path+'\myfundStatistics.xlsx')
 
 def runTaskRegularTime():
 	while True:
@@ -56,9 +60,9 @@ def taskWait(currentTime):
 	time.sleep(waitTime)
 
 #调接口查询
-def inquiryRate(shourNumber):
+def inquiryRate(shortNumber):
 	try:
-		url = (" http://fundmobapi.eastmoney.com/FundMApi/FundVarietieValuationDetail.ashx?version=5.3.0&plat=Android&appType=ttjj&FCODE=%s&deviceid=e19655bcbef4c4b362d908f8bcbdd67f||946596830144568&product=EFund&MobileKey=e19655bcbef4c4b362d908f8bcbdd67f||946596830144568"%shourNumber)
+		url = (" http://fundmobapi.eastmoney.com/FundMApi/FundVarietieValuationDetail.ashx?version=5.3.0&plat=Android&appType=ttjj&FCODE=%s&deviceid=e19655bcbef4c4b362d908f8bcbdd67f||946596830144568&product=EFund&MobileKey=e19655bcbef4c4b362d908f8bcbdd67f||946596830144568" % shortNumber)
 		r = requests.get(url, params=None)
 		# print(r.json()["Expansion"])
 		today = r.json()["Expansion"]["GZTIME"][0:10]#只截取到日期，不要时间
@@ -97,7 +101,7 @@ def sendEmail(list):
 
 #读取项目的excel，修改添加数据后保存
 def write_excel(list):
-	wb = load_workbook('myfundStatistics.xlsx')
+	wb = load_workbook(excel_path)
 	sheet = wb['Sheet1']
 	n_coordinate = 0
 	for row in sheet.iter_rows():#找到空白行号
@@ -109,7 +113,7 @@ def write_excel(list):
 	for n in range(len(list)):#通过遍历给cell赋值
 		for m in range(len(list[0])):
 			sheet.cell(n_coordinate+n,m+1).value=list[n][m]
-	wb.save('myfundStatistics.xlsx')
+	wb.save(excel_path)
 
 if __name__ == '__main__':
 	runTaskRegularTime()
